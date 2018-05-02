295d4714d577c1223954e66fd3d88729fb543447
Vasily Karmazin <spore0072lol@rambler.ru>
2018-05-03 02:58:41 +0300

    kommivoyajer

---
+++ Salesman/Salesman/Matrix.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salesman
+{
+    class Matrix
+    {
+        public int[,] m;
+        public int n;
+
+        public List<int> vi = new List<int>();
+        public List<int> vj = new List<int>();
+
+        public List<int> vvi = new List<int>();
+        public List<int> vvj = new List<int>();
+
+        private int[] hi, hj;
+
+        public int indexI, indexJ, H = 0;
+        public bool negative;
+
+        public Matrix(int[,] m)
+        {
+            indexI = indexJ = 0;
+            negative = true;
+
+            this.m = (int[,]) m.Clone();
+            n = m.GetLength(0);
+            hi = new int[n];
+            hj = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vi.Add(i);
+                vj.Add(i);
+            }
+        }
+
+        public Matrix(Matrix mat)
+        {
+            indexI = mat.indexI;
+            indexJ = mat.indexJ;
+            negative = mat.negative;
+
+            n = mat.n;
+            hi = new int[n];
+            hj = new int[n];
+            m = new int[n, n];
+
+            H = mat.H;
+
+            m = (int[,]) mat.m.Clone();
+            
+            vi = mat.vi.ToList();
+            vj = mat.vj.ToList();
+
+            vvi = mat.vvi.ToList();
+            vvj = mat.vvj.ToList();
+        }
+
+        public Matrix[] Solve()
+        {
+            Matrix[] mat = new Matrix[2];
+            
+            PrintTable();
+            FindMinJ();
+            FindMinI();
+            PrintTable();
+
+            H += FindH();
+            int H0 = H;
+            Console.WriteLine("H0 = " + H);
+            FindIndex();     
+
+            m[indexI, indexJ] = Program.inf;
+            FindMinJ();
+            FindMinI();
+            H += FindH();
+            Console.WriteLine("\nH* = " + H);
+            negative = false;
+            mat[0] = new Matrix(this);
+            PrintTable();
+            H = H0;
+
+            vi.Remove(indexI);
+            vj.Remove(indexJ);
+
+
+            vvi.Add(indexI);
+            vvj.Add(indexJ);
+            /*
+            for (int x = 0; x < n; x++)
+            {
+                if (vvj.Contains(x))
+                { 
+                    for (int y = 0; y < n; y++)
+                    {
+                        if (vvi.Contains(y) && x != y)
+                        {
+                            m[x, y] = Program.inf;
+                        }
+                    }
+                }
+            }*/
+
+            m[indexJ, indexI] = Program.inf;
+            FindMinJ();
+            FindMinI();
+            H += FindH();
+            Console.WriteLine("H = " + H);
+            negative = true;
+            mat[1] = new Matrix(this);
+            PrintTable(); 
+            H = H0;
+
+            return mat;
+        }
+
+        private int FindH()
+        {
+            int H = 0;
+            foreach (int i in vi)
+            {
+                H += hj[i];
+            }
+            foreach (int j in vj)
+            {
+                H += hi[j];
+            }
+            return H;
+        }
+
+        private void FindMinJ()
+        {
+            foreach (int i in vi)
+            {
+                hj[i] = Program.inf;
+
+                foreach (int j in vj)
+                {
+                    if (hj[i] > m[i, j])
+                    {
+                        hj[i] = m[i, j];
+                    }
+                }
+
+                foreach (int j in vj)
+                {
+                    if (m[i, j] != Program.inf)
+                    {
+                        m[i, j] -= hj[i];
+                    }
+                }
+            }
+        }
+
+        private void FindMinI()
+        {
+            foreach (int j in vj)
+            {
+                hi[j] = Program.inf;
+
+                foreach (int i in vi)
+                {
+                    if (hi[j] > m[i, j])
+                    {
+                        hi[j] = m[i, j];
+                    }
+                }
+
+                foreach (int i in vi)
+                {
+                    if (m[i, j] != Program.inf)
+                    {
+                        m[i, j] -= hi[j];
+                    }
+                }
+            }
+        }
+
+        private void FindIndex()
+        {
+            int D = 0;
+            foreach (int i in vi)
+            {
+                foreach (int j in vj)
+                {
+                    if (m[i, j] == 0)
+                    {
+                        Console.WriteLine("D(" + (i + 1) + ";" + (j + 1) + ") = " + FindD(i, j));
+                        if (D < FindD(i, j))
+                        {
+                            D = FindD(i, j);
+                            indexI = i;
+                            indexJ = j;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Remove: (" + (indexI + 1) + ";" + (indexJ + 1) + ")");
+        }
+
+        private int FindD(int x, int y)
+        {
+            int min = Program.inf, D = 0;
+            foreach (int i in vi)
+            {
+                if (i != x && min > m[i, y])
+                {
+                    min = m[i, y];
+                }
+            }
+            D += min;
+
+            min = Program.inf;
+            foreach (int j in vj)
+            {
+                if (j != y && min > m[x, j])
+                {
+                    min = m[x, j];
+                }
+            }
+            D += min;
+
+            return D;
+        }
+
+        public void PrintTable()
+        {
+            Console.Write(String.Format("{0,-4}", ""));
+            foreach (int j in vj)
+            {
+                Console.Write(String.Format("{0,-4}", "v" + (j + 1)));
+            }
+            Console.WriteLine(String.Format("{0,-4}", "hj"));
+
+            foreach (int i in vi)
+            {
+                Console.Write(String.Format("{0,-4}", "v" + (i + 1)));
+                foreach (int j in vj)
+                {
+                    if (m[i, j] == Program.inf)
+                    {
+                        Console.Write(String.Format("{0,-4}", "inf"));
+                    }
+                    else
+                    {
+                        Console.Write(String.Format("{0,-4}", m[i, j]));
+                    }
+                }
+                Console.WriteLine(String.Format("{0,-4}", hj[i]));
+            }
+
+            Console.Write(String.Format("{0,-4}", "hi"));
+            foreach (int j in vj)
+            {
+                Console.Write(String.Format("{0,-4}", hi[j]));
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ Salesman/Salesman/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salesman
+{
+    class Program
+    {
+        public static int inf = Int32.MaxValue / 2;
+        public static List<Matrix> list = new List<Matrix>();
+        /*
+        public static int[,] matrix = new int[,]
+            {{ inf, 68,  73,  24,  70,  9 },
+             { 58,  inf, 16,  44,  11,  92 },
+             { 63,  9,   inf, 86,  13,  18 },
+             { 17,  34,  76,  inf, 52,  70 },
+             { 60,  18,  3,   45,  inf, 58 },
+             { 16,  82,  11,  60,  48,  inf }};
+        */
+        public static int[,] matrix = new int[,]
+            {{ inf, 7,   12,  25,  10 },
+             { 10,  inf, 9,   5,   11 },
+             { 13,  8,   inf, 6,   4 },
+             { 6,   11,  15,  inf, 15 },
+             { 5,   9,   12,  17,  inf }};
+        
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix(matrix);
+            Matrix[] res;
+
+            while(true)
+            {
+                Console.WriteLine("\t-Step-");
+
+                res = m.Solve();
+                list.Add(res[0]);
+                list.Add(res[1]);
+
+                m = new Matrix(res[0]);
+
+                foreach (Matrix mat in list)
+                {
+                    Console.WriteLine("NEXT (" + (mat.indexI + 1) + "," + (mat.indexJ + 1) + ")" + ((mat.negative) ? "" : "*") + " : " + mat.H);
+                    if (m.H > mat.H)
+                    {
+                        m = new Matrix(mat);
+                    }
+                }
+
+                Remove(m.indexI, m.indexJ, m.negative);
+
+                Console.WriteLine("Selected (" + (m.indexI + 1) + "," + (m.indexJ + 1) + ")" + ((m.negative) ? "" : "*") + " : " + m.H + "\n");
+
+                if (m.vi.Count == 2)
+                {
+                    m.vvi.Add(m.vi[0]);
+                    m.vvi.Add(m.vi[1]);
+                    m.vvj.Add(m.vj[1]);
+                    m.vvj.Add(m.vj[0]);
+                    break;
+                }    
+            }
+            Console.Write("Way : ");
+            int way = 0;
+            for (int x = 0; x < m.n; x++)
+            {
+                Console.Write("(" + (m.vvi[x] + 1) + "," + (m.vvj[x] + 1) + ") ");
+                way += matrix[m.vvi[x], m.vvj[x]];
+            }
+            Console.WriteLine("\nWay = " + way);
+            Console.Read();
+        }
+
+        public static void Remove(int i, int j, bool negative)
+        {
+            foreach (Matrix m in list)
+            {
+                if (m.indexI == i && m.indexJ == j && m.negative == negative)
+                {
+                    list.Remove(m);
+                    break;
+                }
+            }
+        }
+    }
+}
